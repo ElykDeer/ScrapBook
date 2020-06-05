665ef03b722e82ac891692ddb974b37e391f8307
KyleMiles <krm504@nyu.edu>
2020-06-05 01:13:45 -0400

    Fine-tune ISO script

--- Ubuntu-ISO-Cloner/rsync-ubuntu-releases.sh
+++ Ubuntu-ISO-Cloner/rsync-ubuntu-releases.sh
@@ -5,18 +5,24 @@ fatal() {
   exit 1
 }
 
-warn() {
-  echo "$1"
-}
+BASEDIR=rsync://us.rsync.releases.ubuntu.com/releases/.pool/
 
 # Define where you want the mirror-data to be on your mirror
-BASEDIR=/path/to/save/isos
+SAVEDIR=/path/to/save/isos/in
 
-if [ ! -d ${BASEDIR} ]; then
-  fatal "Save destination does not exist"
+if [ ! -d ${SAVEDIR} ]; then
+  fatal "Save destination does no exist"
 fi
 
-rsync --verbose --times --stats --recursive --delete-after -h rsync://us.rsync.releases.ubuntu.com/releases/.pool/*.04*{server,desktop}*amd64.iso ${BASEDIR} || fatal "Failed to rsync from ${RSYNCSOURCE}."
+
+# Adjust hopw many files we want
+FILES=$(rsync --list-only ${BASEDIR}*.04*{server,desktop}*amd64.iso . | sed -n -e 's/^.* \(\S*\)$/\1/p' | sort -n | tail -6)
+
+for FILENAME in ${FILES}
+do
+  echo ${BASEDIR}${FILENAME}
+  rsync --verbose --times --stats --recursive --delete-after -h --progress ${BASEDIR}${FILENAME} ${SAVEDIR} || fatal "Failed to rsync from ${RSYNCSOURCE}."
+done
 
 # Ubuntu suggests release mirrors to check every four hours, for example:
-# 0 */4 * * * "/path/to/rsync-ubuntu-releases.sh" > /dev/null
+# 0 */4 * * * /root/rsync-ubuntu-releases.sh > /root/rsync-ubuntu-releases.log
